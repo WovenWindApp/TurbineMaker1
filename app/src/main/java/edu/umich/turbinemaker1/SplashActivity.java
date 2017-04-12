7302d0a89bcb170f1b13dd5acdaa1b0d12716c08
Dylan Lynch <dqlynch@umich.edu>
2017-04-11 20:28:12 -0400

    Adjustable structure frame
    hacky nested linear layouts to change width and height

--- app/src/main/java/edu/umich/turbinemaker1/SplashActivity.java
+++ app/src/main/java/edu/umich/turbinemaker1/SplashActivity.java
@@ -2,13 +2,20 @@ package edu.umich.turbinemaker1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Force landscape mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         Intent intent = new Intent(this, MainScreen.class);
         startActivity(intent);
--- app/src/main/java/edu/umich/turbinemaker1/TurbineDesign.java
+++ app/src/main/java/edu/umich/turbinemaker1/TurbineDesign.java
@@ -1,7 +1,11 @@
 package edu.umich.turbinemaker1;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class TurbineDesign extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class TurbineDesign extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turbine_design);
+
+        View v = findViewById(android.R.id.content);
+
+
+        // Force landscape mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setUpStructure();
+
+
+
+
+    }
+
+
+    void setUpStructure() {
+        ImageView structure = (ImageView) findViewById(R.id.turbine_design_structure);
+
+        setUpStructureWidthSlider(structure);
+
+
+    }
+
+
+    void setUpStructureWidthSlider(ImageView structure) {
+        
+
+
     }
 }
