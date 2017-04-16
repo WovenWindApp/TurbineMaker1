bdb00c8977199020bb92ff63ccb9dfe654986ed4
izakfr <izakfr@umich.edu>
2017-04-16 00:56:41 -0400

    This commit adds an about page for the user to click on and view details about the app

---
+++ app/src/main/java/edu/umich/turbinemaker1/AboutPage.java
@@ -0,0 +1,13 @@
+package edu.umich.turbinemaker1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_page);
+    }
+}
